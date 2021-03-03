beaa44dfe90ec2305bd9ef39fe7c52e46a086507
shu0405 <75825152+shu0405@users.noreply.github.com>
2021-03-03 11:24:02 +0900

    ログイン条件分岐の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
   
   private
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :state_id, :burden_id, :area_id, :day_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :state_id, :burden_id, :area_id, :day_id, :price, :image).merge(user_id: current_user.id)
   end
 
   def set_category_column
