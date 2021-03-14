156a874b355750b5284df4a7b2eafb7031b10be4
shu0405 <75825152+shu0405@users.noreply.github.com>
2021-03-14 17:47:30 +0900

    商品詳細表示機能レビュー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.order("created_at DESC")
@@ -20,6 +20,10 @@ class ItemsController < ApplicationController
   end
 
   def edit
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
   
   private
